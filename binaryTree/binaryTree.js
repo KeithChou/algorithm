96c57b25064dc1d3dac1993217dee40f662237bc
KeithChou <keithchou95@gmail.com>
2017-09-10 14:58:37 +0800

    实现深度优先遍历算法：前序、中序、后序

--- binaryTree/binaryTree.js
+++ binaryTree/binaryTree.js
@@ -1,6 +1,9 @@
 const Algorithm = require('../index.js')
 const insert = Symbol('insert')
 const insertNode = Symbol('insertNode')
+const inOrderTraverseNode = Symbol('inOrderTraverseNode')
+const prevOrderTraverseNode = Symbol('prevOrderTraverseNode')
+const nextOrderTraverseNode = Symbol('nextOrderTraverseNode')
 
 class Node {
   constructor (key) {
@@ -15,7 +18,7 @@ class BinaryTree extends Algorithm {
     super(option)
     this.root = null
   }
-  [insert] (key) {
+  [insert] (key, cb) {
     let newNode = new Node(key)
     if (this.root === null) {
       this.root = newNode
@@ -23,7 +26,7 @@ class BinaryTree extends Algorithm {
       this[insertNode](this.root, newNode)
     }
     // return this.root
-    console.log(this.root)
+    cb(this.root)
   }
   [insertNode] (node, newNode) {
     if (newNode.key < node.key) {
@@ -40,9 +43,51 @@ class BinaryTree extends Algorithm {
       }
     }
   }
-  createBinaryTree (arr = [8, 3, 10, 1, 6, 14, 4, 7, 13]) {
+  [inOrderTraverseNode] (node, callback) {
+    if (node !== null) {
+      this[inOrderTraverseNode](node.left, callback)
+      callback(node.key)
+      this[inOrderTraverseNode](node.right, callback)
+    }
+  }
+  [prevOrderTraverseNode] (node, callback) {
+    if (node !== null) {
+      callback(node.key)
+      this[prevOrderTraverseNode](node.left, callback)
+      this[prevOrderTraverseNode](node.right, callback)
+    }
+  }
+  [nextOrderTraverseNode] (node, callback) {
+    if (node !== null) {
+      this[nextOrderTraverseNode](node.left, callback)
+      this[nextOrderTraverseNode](node.right, callback)
+      callback(node.key)
+    }
+  }
+  createBinaryTree (arr = [8, 3, 10, 1, 6, 14, 4, 7, 13], cb = () => {}) {
+    if (typeof arr === 'function') {
+      cb = arr
+      arr = [8, 3, 10, 1, 6, 14, 4, 7, 13]
+    }
     arr.forEach(value => {
-      this[insert](value)
+      this[insert](value, cb)
     })
   }
+  inOrderTraverse (callback = () => {}) {
+    this.createBinaryTree()
+    this[inOrderTraverseNode](this.root, callback)
+  }
+  prevOrderTraverse (callback = () => {}) {
+    this.createBinaryTree()
+    this[prevOrderTraverseNode](this.root, callback)
+  }
+  nextOrderTraverse (callback = () => {}) {
+    this.createBinaryTree()
+    this[nextOrderTraverseNode](this.root, callback)
+  }
 }
+
+let a = new BinaryTree()
+a.nextOrderTraverse(key => {
+  console.log(key)
+})
