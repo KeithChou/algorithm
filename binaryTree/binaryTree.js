17f7345f9dc7da62d5c63510e6c402a0183f85a2
KeithChou <keithchou95@gmail.com>
2017-09-10 17:27:12 +0800

    结点改为节点..

--- binaryTree/binaryTree.js
+++ binaryTree/binaryTree.js
@@ -118,27 +118,27 @@ class BinaryTree extends Algorithm {
       node.right = this[removeNode](node.right, key)
       return node
     } else {
-      // 删除二叉树中的结点分四种情况：
-      // 1. 该结点有左右孩子；2. 该结点没有左右孩子；3. 该结点只有左孩子；4. 该结点只有右孩子
+      // 删除二叉树中的节点分四种情况：
+      // 1. 该节点有左右孩子；2. 该节点没有左右孩子；3. 该节点只有左孩子；4. 该节点只有右孩子
 
-      // 1. 该结点有左右孩子，此时要找到该结点右子树中的最小值，然后将该结点的值等于右子树中最小值。最后删除该最小值的结点。
+      // 1. 该节点有左右孩子，此时要找到该节点右子树中的最小值，然后将该节点的值等于右子树中最小值。最后删除该最小值的节点。
       if (node.left !== null && node.right !== null) {
         let min = this[findMinNode](node.right)
         node.key = min.key
         node.right = this[removeNode](node.right, min.key)
         return node
       }
-      // 2. 该结点没有左右孩子：则直接删除该结点(将其置为null即删除)
+      // 2. 该节点没有左右孩子：则直接删除该节点(将其置为null即删除)
       if (node.left === null && node.right === null) {
         node = null
         return node
       }
-      // 3. 该结点只有左孩子：将该结点等于该结点的左孩子。
+      // 3. 该节点只有左孩子：将该节点等于该节点的左孩子。
       if (node.right === null) {
         node = node.left
         return node
       }
-      // 4. 该结点只有右孩子：将该结点等于该结点的右孩子。
+      // 4. 该节点只有右孩子：将该节点等于该节点的右孩子。
       if (node.left === null) {
         node = node.right
         return node
@@ -154,7 +154,7 @@ class BinaryTree extends Algorithm {
       this[insert](value, cb)
     })
   }
-  // 中序遍历二叉树, callback接受一个参数，表示二叉树中的每个结点指。先序，后序相同。
+  // 中序遍历二叉树, callback接受一个参数，表示二叉树中的每个节点指。先序，后序相同。
   // 用途：将二叉树转换成双向链表、从小到大排序二叉树
   inOrderTraverse (callback = () => {}) {
     this[inOrderTraverseNode](this.root, callback)
@@ -182,12 +182,12 @@ class BinaryTree extends Algorithm {
     return node
   }
   // 在二叉树中找到指定的值，若存在，则返回true；否则返回false
-  // 原理：与当前父结点的key对比，递归遍历searchNode方法
+  // 原理：与当前父节点的key对比，递归遍历searchNode方法
   search (key) {
     let node = this[searchNode](this.root, key)
     return node
   }
-  // 删除二叉树中的某个结点，返回删除结点之后的二叉树。如果找不到此结点，则返回结点二叉树本身(相当于复制二叉树)
+  // 删除二叉树中的某个节点，返回删除节点之后的二叉树。如果找不到此节点，则返回节点二叉树本身(相当于复制二叉树)
   remove (key) {
     let node = this[removeNode](this.root, key)
     return node
