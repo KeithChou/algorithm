0045687e9d617b0d76a28b10b3e3358d4158f9ad
KeithChou <keithchou95@gmail.com>
2017-09-10 15:39:02 +0800

    在二叉树中找到最小结点、最大结点、找到任意结点

--- binaryTree/binaryTree.js
+++ binaryTree/binaryTree.js
@@ -4,6 +4,9 @@ const insertNode = Symbol('insertNode')
 const inOrderTraverseNode = Symbol('inOrderTraverseNode')
 const prevOrderTraverseNode = Symbol('prevOrderTraverseNode')
 const nextOrderTraverseNode = Symbol('nextOrderTraverseNode')
+const searchMinNode = Symbol('searchMinNode')
+const searchMaxNode = Symbol('searchMaxNode')
+const searchNode = Symbol('searchNode')
 
 class Node {
   constructor (key) {
@@ -25,7 +28,6 @@ class BinaryTree extends Algorithm {
     } else {
       this[insertNode](this.root, newNode)
     }
-    // return this.root
     cb(this.root)
   }
   [insertNode] (node, newNode) {
@@ -64,6 +66,36 @@ class BinaryTree extends Algorithm {
       callback(node.key)
     }
   }
+  [searchMinNode] (node) {
+    if (node === null) {
+       return false
+    }
+    while (node && node.left !== null) {
+      node = node.left
+    }
+    return node.key
+  }
+  [searchMaxNode] (node) {
+    if (node === null) {
+      return false
+    }
+    while (node && node.right !== null) {
+      node = node.right
+    }
+    return node.key
+  }
+  [searchNode] (node, key) {
+    if (node === null) {
+      return false
+    }
+    if (key < node.key) {
+      return this[searchNode](node.left, key)
+    } else if (key > node.key) {
+      return this[searchNode](node.right, key)
+    } else {
+      return true
+    }
+  }
   createBinaryTree (arr = [8, 3, 10, 1, 6, 14, 4, 7, 13], cb = () => {}) {
     if (typeof arr === 'function') {
       cb = arr
@@ -73,21 +105,31 @@ class BinaryTree extends Algorithm {
       this[insert](value, cb)
     })
   }
+  // 中序遍历二叉树, callback接受一个参数，表示二叉树中的每个节点指。先序，后续相同。
   inOrderTraverse (callback = () => {}) {
-    this.createBinaryTree()
     this[inOrderTraverseNode](this.root, callback)
   }
+  // 先序遍历二叉树
   prevOrderTraverse (callback = () => {}) {
-    this.createBinaryTree()
     this[prevOrderTraverseNode](this.root, callback)
   }
+  // 后续遍历二叉树
   nextOrderTraverse (callback = () => {}) {
-    this.createBinaryTree()
     this[nextOrderTraverseNode](this.root, callback)
   }
+  // 找到二叉树中的最小值，返回最小值
+  searchMin () {
+    let node = this[searchMinNode](this.root)
+    return node
+  }
+  // 找到二叉树中的最大值，返回最大值
+  searchMax () {
+    let node = this[searchMaxNode](this.root)
+    return node
+  }
+  // 在二叉树中找到指定的值，若存在，则返回true；否则返回false
+  search (key) {
+    let node = this[searchNode](this.root, key)
+    return node
+  }
 }
-
-let a = new BinaryTree()
-a.nextOrderTraverse(key => {
-  console.log(key)
-})
